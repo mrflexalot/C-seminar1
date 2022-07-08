2667aa4f9cee984e4ca16e6333cd1c7eed7ed24e
IgorZagrebelny <igor6230450@gmail.com>
2022-07-08 11:34:14 +0300

    решили вторую задачу

--- 1_lesson/1_1/Program.cs
+++ 1_lesson/1_1/Program.cs
@@ -1,5 +1,4 @@
 ﻿Console.WriteLine("Write a number: ");
-string s_a = Console.ReadLine();
 
-int a = int.Parse(s_a);
+int a = int.Parse(Console.ReadLine());
 Console.WriteLine(a * a);
---
+++ 1_lesson/1_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (b * b==a)
+Console.WriteLine("Да");
+else
+Console.WriteLine("Нет");
