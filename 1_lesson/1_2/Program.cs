dd241065d0c2ced57d4e10121550d16e449054e7
IgorZagrebelny <igor6230450@gmail.com>
2022-07-08 11:45:55 +0300

    решили 3 задачу

--- 1_lesson/1_2/Program.cs
+++ 1_lesson/1_2/Program.cs
@@ -4,7 +4,7 @@ int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Write a number b: ");
 int b = int.Parse(Console.ReadLine());
 
-if (b * b==a)
-Console.WriteLine("Да");
+if (b * b == a)
+    Console.WriteLine("Да");
 else
-Console.WriteLine("Нет");
+    Console.WriteLine("Нет");
