cf37b4463da1fff55504727810f2981baa4cb693
IgorZagrebelny <igor6230450@gmail.com>
2022-07-11 13:23:20 +0300

    initital homework commit

--- 1_lesson/1_4/Program.cs
+++ 1_lesson/1_4/Program.cs
@@ -1,8 +1,11 @@
-﻿Console.WriteLine("Write a number a: ");
+﻿// 4. Напишите программу, которая на вход принимает одно число
+//    (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Write a number a: ");
 int a = int.Parse(Console.ReadLine());
-// if (a<0)
-int i=-a;
-while (i<=a)
+// if (a < 0)
+int i = -a;
+while (i <= a)
 {
     Console.WriteLine(i);
     i++;
