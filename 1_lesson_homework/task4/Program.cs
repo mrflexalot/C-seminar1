b5e28ad3535b6768fea1d42a8c583a2fc76b9a97
IgorZagrebelny <igor6230450@gmail.com>
2022-07-11 15:42:00 +0300

    completed task4

---
+++ 1_lesson_homework/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 7: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+//456 -> 6
+//782 -> 2
+//918 -> 8
+
+Console.WriteLine("Write a number A: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a >= 100) 
+    {Console.WriteLine("the last number of A is:");
+    Console.WriteLine(a%10);}
+    
+    else Console.WriteLine("invalid number");
