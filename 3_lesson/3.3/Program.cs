675e04c0f47a7eaa6059e8a7baf21514009aca17
IgorZagrebelny <igor6230450@gmail.com>
2022-07-15 11:00:18 +0300

    completed 3d

---
+++ 3_lesson/3.3/Program.cs
@@ -0,0 +1,7 @@
+﻿double Distance(double x1, double y1, double x2, double y2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)), 4);
+}
+
+Distance(3, 6, 2, 1);
+Console.WriteLine(Distance(3, 6, 2, 1));
