6c68a85cb52bac5fa4253767c2b85be2abeb28eb
IgorZagrebelny <igor6230450@gmail.com>
2022-07-17 19:33:12 +0300

    completed 1st homework task

---
+++ 2_lesson_homework/2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+int result(int a)
+{
+    return (((a % 100) - (a % 10)) / 10);
+}
+Console.WriteLine("enter 3-digit number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"second digit of your number is: {result(a)}");
