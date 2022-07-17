20b80e822fe9f649f5a0f2d053a3f6c8da657c35
IgorZagrebelny <igor6230450@gmail.com>
2022-07-17 20:50:16 +0300

    completed 3d hw task

---
+++ 2_lesson_homework/2.3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+Console.WriteLine("enter your number:");
+int a = int.Parse(Console.ReadLine());
+void result(int a)
+{
+    if (a < 100)
+    {
+        Console.WriteLine($"{a} has no third number!");
+        return;
+    }
+    while (a > 999)
+        a = a / 10;
+    Console.WriteLine(a % 10);
+}
+result(a);
