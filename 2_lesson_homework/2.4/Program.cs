84f86b1a7a1b71e4b2690692343bdb95b8c2ac4a
IgorZagrebelny <igor6230450@gmail.com>
2022-07-17 21:37:09 +0300

    completed 4th hw task

---
+++ 2_lesson_homework/2.4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+Console.WriteLine("enter the number from 1 to 7 to check if it's a weekend or not:");
+int a = int.Parse(Console.ReadLine());
+if (a > 7)
+{
+    Console.WriteLine("Error");
+}
+else if (a > 5 )
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
