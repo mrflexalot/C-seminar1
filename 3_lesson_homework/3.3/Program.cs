90a1102c84e615c97a5c551aff1e6f75f3afad46
IgorZagrebelny <igor6230450@gmail.com>
2022-07-22 09:42:39 +0300

    completed 3rd hw task

---
+++ 3_lesson_homework/3.3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27.
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("enter a number to form a 3rd power list of it:");
+int N = int.Parse(Console.ReadLine());
+void result(int N)
+{
+    if (N < 1)
+    {
+        Console.WriteLine("Incorrect number");
+        return;
+    }
+    int i = 1;
+    while (i <= N)
+    {
+        Console.WriteLine(Math.Pow(i, 3));
+        i++;
+    }
+}
+Console.WriteLine($"Your 3rd power list of number {N}:");
+result(N);
