3e3f34ac2b23cf79d26968a5feb8d5ff9c648b42
IgorZagrebelny <igor6230450@gmail.com>
2022-08-04 12:27:46 +0300

    completed 4d last task

---
+++ 4_lesson/4_3/Program.cs
@@ -0,0 +1,10 @@
+﻿void RandomArray(int num)
+{
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i]=new Random().Next(0, 2);
+        Console.Write($"{array[i]} ");
+    }
+}
+RandomArray(8);
