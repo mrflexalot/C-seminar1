55e8e78c7d8cfb30d89211932f91aba35c8598ba
IgorZagrebelny <igor6230450@gmail.com>
2022-08-05 10:53:32 +0300

    completed 2d task

--- 5_lesson/5_1/Program.cs
+++ 5_lesson/5_1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1. Напишите программу замена элементов массива:
+//    положительные элементы замените на соответствующие отрицательные, и наоборот.
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size, int from, int to)
+{   
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);        
+    }    
+    return arr;
+}
+
+void Ism(int[] arr)
+{
+
+    for(int i = 0; i < arr.Length; i++)
+
+        arr[i] = arr[i] * -1;
+       
+}
+
+int[] arr_1 = MassNums(12, -9, 10);
+Print(arr_1);
+Ism(arr_1);
+Print(arr_1);
