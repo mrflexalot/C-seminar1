27e62a032d24b1115fcb6b53b9066e7dc6b38626
IgorZagrebelny <igor6230450@gmail.com>
2022-08-05 11:27:48 +0300

    completed 4d task

---
+++ 5_lesson/5_3/Program.cs
@@ -0,0 +1,45 @@
+﻿// 3. Задайте одномерный массив из 123 случайных чисел.
+//    Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+int[] RandomMegaSuperStrongArray()
+{
+    Console.WriteLine("Input number of elements");
+    int num = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input elemet's min");
+    int min = int.Parse(Console.ReadLine());
+    Console.WriteLine("Input elemet's max");
+    int max = int.Parse(Console.ReadLine());
+    int[] array = new int[num];
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+        Console.Write($"{array[i]} ");
+    }
+    return (array);
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int Nums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+            count++;
+    }
+    return count;
+
+}
+
+int[] arr_1 = RandomMegaSuperStrongArray();
+Print(arr_1);
+Console.WriteLine(Nums(arr_1));
