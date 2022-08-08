4883af33629e899a27c517dba45f268ba861f02a
IgorZagrebelny <igor6230450@gmail.com>
2022-08-08 14:37:27 +0300

    completed last 3rd hw task

---
+++ 4_lesson_homework/4_3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+int[] Array8()
+{
+    int size = 8;
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = new Random().Next(1, 999);
+    return arr;
+}
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+}
+
+Print(Array8());
