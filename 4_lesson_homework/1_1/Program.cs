88052cce6616eb8f0c000b7fdcd9933c0e07acc4
IgorZagrebelny <igor6230450@gmail.com>
2022-08-08 13:13:26 +0300

    completed 1st homework task

---
+++ 4_lesson_homework/1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+Console.WriteLine("Enter number a:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number b:");
+int b = int.Parse(Console.ReadLine());
+int exp (int a, int b)
+{
+    int degree = a;
+    for (int i = 2; i <= b; i++)
+    {
+        degree = degree * a;
+    }
+    return degree;
+}
+Console.WriteLine($"Your exponentation result is {exp(a, b)}");
