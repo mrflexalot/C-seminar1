0fff84702aa9394bc2e41e072fce0a3fdd8d9a3e
IgorZagrebelny <igor6230450@gmail.com>
2022-08-08 13:39:05 +0300

    completed 2d hw task

---
+++ 4_lesson_homework/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.WriteLine("Enter the number:");
+int n = int.Parse(Console.ReadLine());
+int Sum(int n)
+{
+    int n_sum = 0;
+    for (int i = n; n != 0; n /= 10)
+    {
+        n_sum += n % 10;
+    }
+    return n_sum;
+}
+Console.WriteLine(Sum(n));
