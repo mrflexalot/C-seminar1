92d4aad16674e1b3301dd6684bb660ef46fa9258
IgorZagrebelny <igor6230450@gmail.com>
2022-08-09 10:56:53 +0300

    completed 2d task

---
+++ 6_lesson/6_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// 2. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string ChangeNum(int num)
+{
+    string result = "";
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num /= 2;
+    }
+    return result;
+}
+Console.WriteLine(ChangeNum(12));
