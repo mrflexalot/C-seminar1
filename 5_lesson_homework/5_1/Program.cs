a3b784bf9d06b1a4034e8f8a225fb1cb6fbfff96
IgorZagrebelny <igor6230450@gmail.com>
2022-08-11 13:25:20 +0300

    completed 1st hw task

---
+++ 5_lesson_homework/5_1/Program.cs
@@ -0,0 +1,38 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+int[] RandomArray(int size, int from, int to)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+int EvenNum(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count += 1;
+        }
+
+    }
+    return count;
+}
+int[] arr = RandomArray(15, 100, 1000);
+Print(arr);
+Console.WriteLine($"Number of even numbers is: {(EvenNum(arr))}");
