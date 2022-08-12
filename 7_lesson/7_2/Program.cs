b30d41ab4fd23a1e35c0be361bea4a26f95d08ca
IgorZagrebelny <igor6230450@gmail.com>
2022-08-12 10:58:35 +0300

    completed 3rd task

---
+++ 7_lesson/7_2/Program.cs
@@ -0,0 +1,35 @@
+﻿void Print2xArray(int[,] arr)
+{
+    int[,] Fill2xArray(int row, int column, int min, int max)
+    {
+        int[,] arr = new int[row, column];
+
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < column; j++)
+            {
+                arr[i, j] = new Random().Next(min, max + 1);
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        return (arr);
+    }
+}
+int[,] ReplaceElOn2ndPos2xArray(int[,] arr)
+{
+    for (int i = 1; i < arr.GetLength(0); i += 2)
+    {
+        for (int j = 1; j < arr.GetLength(1); j += 2)
+        {
+            arr[i, j] *= arr[i, j];
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return (arr);
+}
+int[,] arr1 = Fill2xArray(4, 4, -10, 10);
+Print2xArray(arr1);
+ReplaceElOn2ndPos2xArray(arr1);
+Print2xArray(arr1);
---
+++ 7_lesson/7_3/Program.cs
@@ -0,0 +1,59 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+    return arr;
+}
+int SumOfDiagonaile (int[,] arr)
+{
+    int count = 0;
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    if (row < column)
+    {
+        column = row;
+    }
+    else if (column < row)
+    {
+        row = column;
+    }
+    for (int i = 0; i < row; i++)
+    {
+        count = count + arr[i, i];
+    }
+    return count;
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 1, 11);
+Print(arr_1);
+
+Console.WriteLine(SumOfDiagonaile(arr_1));
