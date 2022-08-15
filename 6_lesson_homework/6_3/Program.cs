0b4ed32765cc106f24f76506d972c2534648716a
IgorZagrebelny <igor6230450@gmail.com>
2022-08-15 16:35:36 +0300

    completed 3rd hw task

---
+++ 6_lesson_homework/6_3/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая будет создавать копию заданного массива(вещественные числа) с помощью поэлементного копирования.
+void Print(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+double[] MassNums(int size, int from, int to)
+{
+    double[] arr = new double[size];
+    Random n_new = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Math.Round(n_new.NextDouble() * (10 + 20) - 10, 2);
+    }
+    return arr;
+}
+
+double[] Copy(double[] arr)
+{
+    double[] new_arr = new double[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+        new_arr[i] = arr[i];
+    return new_arr;
+}
+double[] arr_1 = MassNums(8, 1, 10);
+Print(arr_1);
+double[] arr_1_new = Copy(arr_1);
+Print(arr_1_new);
