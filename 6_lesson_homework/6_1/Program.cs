2414d70d53a150cd105a9dc81a50b2f768d9a6fe
IgorZagrebelny <igor6230450@gmail.com>
2022-08-15 15:33:28 +0300

    completed 1st hw task

---
+++ 6_lesson_homework/6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+int PosNum(int num)
+{
+    int count = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"Number {i}: ");
+        int a = int.Parse(Console.ReadLine());
+        if (a > 0) count++;
+    }
+    return count;
+}
+Console.Write("How many numbers do you need to check? ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine($"Positive numbers: {PosNum(a)}");
