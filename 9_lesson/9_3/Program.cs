0f36d1c0a7fbdf20bbf066713b175fb28abb0684
IgorZagrebelny <igor6230450@gmail.com>
2022-08-19 11:12:08 +0300

    completed 3rd task on 9 lesson

---
+++ 9_lesson/9_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// 3. Напишите программу, которая будет принимать на вход число
+//    и возвращать сумму его цифр. Использовать рекурсию
+int SumNums(int sum)
+{
+    if (sum == 0) return 0;
+    return SumNums(sum / 10) + sum % 10;
+}
+Console.WriteLine(SumNums(123));
