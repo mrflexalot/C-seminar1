e7a0d25d6fbe9f1397b5afdc198522954dee58c9
IgorZagrebelny <igor6230450@gmail.com>
2022-08-25 11:43:17 +0300

    completed 1st hw task on 9 lesson

---
+++ 9_lesson_homework/9_1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> ""2, 4""
+//M = 4; N = 8. -> ""4, 6, 8""
+void Rec(int m, int n)
+{
+    n -= n % 2;
+    if (n < m) return;
+    Rec(m, n - 1);
+    Console.Write($"{n}, ");
+}
+Console.Write("Enter the number M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter the number N: ");
+int n = int.Parse(Console.ReadLine());
+Rec(m, n);
