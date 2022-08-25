aa786d581f5a9ff49a1a6827909a8120f865dfaa
IgorZagrebelny <igor6230450@gmail.com>
2022-08-25 11:48:34 +0300

    completed 2nd hw task on 9 lesson

---
+++ 9_lesson_homework/9_2/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+int Sum(int m, int n)
+{
+    if (m == n) return n;
+    return Sum(m, n - 1) + n;
+
+}
+Console.Write("Enter the number M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter the number N: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(Sum(m, n));
